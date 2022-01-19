484db04ae6f7f0948a8789d301c88a30be093b35
Nagendrapatel01 <ap146532@gmail.com>
2022-01-19 00:55:54 -0800

    constraint layout complete

---
+++ app/src/main/java/com/example/constrantlasyout/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.constrantlasyout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button2=findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,RagisterActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/constrantlasyout/RagisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.constrantlasyout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RagisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ragister);
+    }
+}
